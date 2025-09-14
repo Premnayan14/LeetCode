193834dc01b6d5b6fbf021f03bafcb3ab08aeb7c
Premnayan14 <premnayan73@gmail.com>
2025-09-14 17:22:33 +0530

    Runtime 0 ms Beats 100.00%  Memory 27.19 MB Beats 80.07%

--- 344. Reverse String.cpp
+++ 344. Reverse String.cpp
@@ -2,8 +2,12 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         int n=s.size();
+             //1st way
         for(int i=0;i<n/2;i++){
             swap(s[i],s[n-i-1]);
         }
+
+             //2nd way 
+        reverse(s.begin(),s.end());
     } 
 };
