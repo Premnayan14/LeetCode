9d70f77b96d3b6a3d215d8ffc4fb7f394867104d
Premnayan14 <premnayan73@gmail.com>
2025-09-18 23:10:44 +0530

    Runtime 0 ms Beats 100.00%  Memory 7.72 MB Beats 83.53%

--- 231. Power of Two.cpp
+++ 231. Power of Two.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-      if(n==0)return 0;
-      while(n%2==0){
-        n/=2;
-      }  
-      if(n==1){
-        return 1;
-      }else return 0;
+    //   if(n==0)return 0;
+    //   while(n%2==0){
+    //     n/=2;
+    //   }  
+    //   if(n==1){
+    //     return 1;
+    //   }else return 0;
+
+    //    int c=0;
+    //     if(n<1)return 0;
+    //     while(n !=0){
+    //         if(n & 1){
+    //             c++;
+    //             if(c==2)return 0;
+    //         }
+    //         n=n>>1;
+    //     }return 1;
+     
+     
+     return n > 0 && (n & (n - 1)) == 0;
     }
 };
