ef1a86030fd95438248ed8b4f11acfca90fe07fb
Premnayan14 <premnayan73@gmail.com>
2025-09-18 22:30:02 +0530

    Runtime 0 ms Beats 100.00%  Memory 8.08 MB Beats 66.99%

---
+++ 1227. Airplane Seat Assignment Probability.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        return (n!=1)?0.5:1.0;
+
+        // double a=1;
+        // if(n==1){
+        //     return a;
+        // }
+        // else{
+        //     return a/2;
+        // }
+    }
+};
