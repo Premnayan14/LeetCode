312a5e83d561cbea0966a7a639405c6d0f61afd7
Premnayan14 <premnayan73@gmail.com>
2025-09-19 20:55:54 +0530

    Runtime 3 ms Beats 60.51% Memory 8.85 MB Beats 77.71%

--- 326. Power of Three.cpp
+++ 326. Power of Three.cpp
@@ -1,10 +1,25 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n<=0)return 0;
-        while(n%3==0){
-            n/=3;
-        }
-        return n==1;
+        
+        // if(n<=0)return 0;
+        // while(n%3==0){
+        //     n/=3;
+        // }
+        // return n==1;
+
+        // return n > 0 && 1162261467 % n == 0;
+        
+        if(n==1)return true;
+        if(n<3) return false;
+        if(!(n&1))return false; 
+        while(n!=3){
+            if(n%3==0){
+                n/=3;
+            }else{
+                return false;
+            }    
+        }return true; 
+    
     }
 };
