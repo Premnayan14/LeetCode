392a284884a63ffea9aafe5662c8be85a3fa3f8d
Premnayan14 <premnayan73@gmail.com>
2025-09-19 21:07:49 +0530

    Runtime 0 ms Beats 100.00%  Memory 8.88 MB Beats 96.26%

---
+++ 2264. Largest 3-Same-Digit Number in String.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        int n=num.length();
+        string ans="";
+        for(int i=0;i<=n-3;i++){
+            if(num[i]==num[i+1] && num[i]==num[i+2]){
+                string cur=num.substr(i,3);
+                if(cur>ans)ans=cur;
+            }
+        }return ans;
+    }
+};
